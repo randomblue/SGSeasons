65a91d6bec166b0e75fcfc37e01c90098558e2e7
randomblue <jpmedallon@gmail.com>
2022-01-13 20:09:17 -0800

    add SeasonDisplay function of winter/summer determination

--- src/index.js
+++ src/index.js
@@ -3,38 +3,26 @@ import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component{
-    // eslint-disable-next-line no-useless-constructor
-    constructor(props) {
-        super(props);
-        //THIS IS THE ONLY TIME we do direct assignemtn
-        //to this.state
-        this.state = {lat: null, errorMessage:''};
+    state = { lat: null, errorMessage: '' };
 
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                this.setState({ lat: position.coords.latitude});
-                console.log(position)
-            },
+componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+        (position) => {this.setState({ lat: position.coords.latitude})},
+        (err) => {this.setState({ errorMessage: err.message})})
+}
 
-            (err) => {
-                this.setState({ errorMessage: err.message});
-                console.log(err)
-            }
-        );
+//React says we have to define render!!
+render() {
+    if(this.state.errorMessage && !this.state.lat) {
+        return <div> Error:{this.state.errorMessage}</div>
     }
 
-    //React says we have to define render!!
-    render() {
-        if(this.state.errorMessage && !this.state.lat) {
-            return <div> Error:{this.state.errorMessage}</div>
-        }
-
-        if (!this.state.errorMessage && this.state.lat) {
-            return <div>Latitude: {this.state.lat}</div>
-        }
-
-        return <div>Loading!</div>
+    if (!this.state.errorMessage && this.state.lat) {
+        return <SeasonDisplay lat={this.state.lat}/>
     }
+
+    return <div>Loading!</div>
+}
 }
 
 ReactDOM.render(<App />,document.querySelector('#root'));
