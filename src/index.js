7cd6f093e847c27553d0ed88559e10d0a5e6e51f
randomblue <jpmedallon@gmail.com>
2022-01-14 10:50:23 -0800

    added message prop as well as defaultProp

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 class App extends React.Component{
     state = { lat: null, errorMessage: '' };
@@ -21,7 +22,7 @@ render() {
         return <SeasonDisplay lat={this.state.lat}/>
     }
 
-    return <div>Loading!</div>
+    return <div><Spinner message="Please accept location request"/></div>
 }
 }
 
